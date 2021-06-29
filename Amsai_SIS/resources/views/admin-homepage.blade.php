4bf601f190b2a1287fbce68594ea6546c7425847
clarizze <jian.clarizze@gmail.com>
2021-06-29 14:35:30 +0800

    Admin Homepage: Student

--- Amsai_SIS/resources/views/admin-homepage.blade.php
+++ Amsai_SIS/resources/views/admin-homepage.blade.php
@@ -12,12 +12,10 @@
     <div class="container">
         <nav>Nav Bar</nav>
         <main>Main Content</main>
-        <div id="sidebar" class="sidebar">
-            <a href="#"><i class="fa fa-dashboard"></i><span> Dashboard</span></a><br>
-            <a href="#"><i class="fa fa-users"></i><span> Student</span></a><br>
-            <a href="#"><i class="fa fa-gear"></i><span> General Settings</span></a>
-            
+        <div>
+            @include(partials.admin-navbar)
         </div>
+        
         <footer>Footer</footer>
     
     </div>
