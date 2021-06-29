18a04b59d622c63565cd36b70c306090ed00f420
clarizze <jian.clarizze@gmail.com>
2021-06-29 14:28:15 +0800

    Admin Homepage Layout

--- Amsai_SIS/resources/views/admin-homepage.blade.php
+++ Amsai_SIS/resources/views/admin-homepage.blade.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/admin.css">
     <title>Admin Homepage</title>
 </head>
 <body>
     <div class="container">
         <nav>Nav Bar</nav>
         <main>Main Content</main>
-        <div id="sidebar">Sidebar</div>
+        <div id="sidebar" class="sidebar">
+            <a href="#"><i class="fa fa-dashboard"></i><span> Dashboard</span></a><br>
+            <a href="#"><i class="fa fa-users"></i><span> Student</span></a><br>
+            <a href="#"><i class="fa fa-gear"></i><span> General Settings</span></a>
+            
+        </div>
         <footer>Footer</footer>
     
     </div>
