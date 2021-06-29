6433705d51e2ca438f908ce6e4b283f5e863e16f
clarizze <jian.clarizze@gmail.com>
2021-06-29 14:35:53 +0800

    Admin Homepage: Student

---
+++ Amsai_SIS/resources/views/partials/admin-sidebar.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div id="sidebar" class="sidebar">
+            <a href="#"><i class="fa fa-dashboard"></i><span> Dashboard</span></a><br>
+            <a href="#"><i class="fa fa-users"></i><span> Student</span></a><br>
+            <a href="#"><i class="fa fa-gear"></i><span> General Settings</span></a>
+            
+        </div>
+</body>
+</html>
